81475c4660f7baac501559711fe8f86d35e161f9
MichaelTseitlin <49906602+MichaelTseitlin@users.noreply.github.com>
2019-04-24 08:56:17 +0300

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Michael Tseitlin on 4/24/19.
+//  Copyright © 2019 Michael Tseitlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
